28d15b54c3029073c3abaf1337ac9b07ab8b93e2
alexdevsprint <alexdevsprint@gmail.com>
2025-07-06 23:09:35 +0300

    commit

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 
+
+
 const app = express();
 
 const PORT = 3000;
@@ -16,6 +18,26 @@ app.use(
 
 app.use(cors());
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Hello world!',
+  });
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.use((err, req, res, next) => {
+    res.status(500).json({
+      message: 'Something went wrong',
+      error: err.message,
+    });
+  });
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
