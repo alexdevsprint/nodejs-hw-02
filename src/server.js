05a26501e78b34a3d290badbf942c2d7f2adb59c
alexdevsprint <alexdevsprint@gmail.com>
2025-07-06 22:57:36 +0300

    commit

--- src/server.js
+++ src/server.js
@@ -1,8 +1,20 @@
 import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
 
 const app = express();
 
 const PORT = 3000;
+
+app.use(
+  pino({
+    transport: {
+      target: 'pino-pretty',
+    },
+  }),
+);
+
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
